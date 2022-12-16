570ace55bbc79ada27f6ff61f594c3e05e0563a4
Ade Maulana Alfaqi <107929610+ademaulanaalfaqi@users.noreply.github.com>
2022-12-17 06:42:33 +0700

    pawanfiber

--- system/resources/views/admin/absensi/show.blade.php
+++ system/resources/views/admin/absensi/show.blade.php
@@ -20,7 +20,7 @@
 @endsection
 @push('script')
     <script type="module">
-        var map = L.map('map').setView([-1.8169204, 109.987781], 13);
+        var map = L.map('map').setView([-1.8498701, 109.9749553], 16);
         
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
